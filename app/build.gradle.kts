ed74cbe08dce6c5f045cb42a5ca237704e82bb88
prabind <prabin.dhakal@metropolia.fi>
2025-01-04 23:59:19 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,116 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("ly.img.android.sdk").version("10.10.0")
+    id("com.google.devtools.ksp")
+}
+
+IMGLY.configure {
+
+    pesdk {
+        enabled(true)
+        licensePath(null)
+    }
+
+    modules {
+        include("ui:core")
+        include("ui:text")
+        include("ui:focus")
+        include("ui:frame")
+        include("ui:brush")
+        include("ui:filter")
+        include("ui:sticker")
+        include("ui:overlay")
+        include("ui:transform")
+        include("ui:adjustment")
+        include("ui:text-design")
+
+        include("backend:serializer")
+        include("backend:headless")
+        include("backend:background-removal")
+        include("backend:sticker-smart")
+
+        include("assets:font-basic")
+        include("assets:frame-basic")
+        include("assets:filter-basic")
+        include("assets:overlay-basic")
+        include("assets:sticker-shapes")
+        include("assets:sticker-emoticons")
+    }
+}
+
+android {
+    namespace = "com.example.kivapix"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.kivapix"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.compose.animation:animation:1.5.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.compose.foundation:foundation:1.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+}
